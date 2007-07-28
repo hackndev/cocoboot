31cac7ff620f6df02eccd2482b714ecb2e6a7db3
bobofdoom <bobofdoom@4cf8e309-b20d-0410-9cb3-b925f7cd04b4>
2007-07-28 01:28:12 +0000

    Cocoboot: traceparse.py: added instruction decoding via objdump.
    git-svn-id: https://hackndev.svn.sourceforge.net/svnroot/hackndev/cocoboot/trunk@1212 4cf8e309-b20d-0410-9cb3-b925f7cd04b4

--- tools/traceparse.py
+++ tools/traceparse.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python
 import struct
 import sys
+import subprocess
+import tempfile
+
+OBJDUMP_CMD = "arm-softfloat-linux-gnueabi-objdump"
+
 if len(sys.argv) < 2:
 	print 'Usage: %s cocoboot.trc' % sys.argv[0]
 	print 'Decodes a raw cocoboot trace dump file'
 	sys.exit(-1)
+
+def decode_instr(instr):
+	tmpf = tempfile.NamedTemporaryFile()
+	tmpf.write(struct.pack('<I', instr))
+	tmpf.flush()
+
+	out = subprocess.Popen([OBJDUMP_CMD, "-D", "-bbinary", "-marm", tmpf.name], stdout=subprocess.PIPE).communicate()[0]
+	print out
+
 
 f = file(sys.argv[1], 'rb')
 while 1:
@@ -20,6 +34,7 @@ while 1:
 			reg = 'PC '
 		elif i == 15:
 			reg = '   '
+			decode_instr(w)
 		else:
 			reg = 'r%-2d' % (i-1)
 		print '%s = %08x %-10d %s' % (reg, w,w, repr(s))
