3f3af3ae915ff5d7ed176c26b1f0c8da836e6fed
Marek <marex@rin.(none)>
2009-02-11 20:25:41 +0100

    Make the demo ELF kernel a bit more interesting

--- elfkernel/kernel.c
+++ elfkernel/kernel.c
@@ -1,10 +1,21 @@
 /* Sample kernel source
  * 
- * This just turns on LED on PalmLD and enters endless loop.
+ * This simply blinks the LEDs on PalmLD.
  * */
+
+#define delay(i)	for(i = 0; i < 0x200000; i++)
 
 void kmain()
 {
-	(*(unsigned long *)0x40e0001c) |= 0x00100000;
-	for(;;);
+	int i;
+	(*(unsigned long *)0x40e00020) &= ~0x40000000;
+	(*(unsigned long *)0x40e0001c) &= ~0x00100000;
+	for (;;) {
+		(*(unsigned long *)0x40e00020) &= ~0x40000000;
+		(*(unsigned long *)0x40e0001c) |= 0x00100000;
+		delay(i);
+		(*(unsigned long *)0x40e0001c) &= ~0x00100000;
+		(*(unsigned long *)0x40e00020) |= 0x40000000;
+		delay(i);
+	}
 }
